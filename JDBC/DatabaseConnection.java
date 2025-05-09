7821374bc056a6c4fd6911b75337202282d84bd8
asliaydin0 <asliaydn12204@gmail.com>
2025-05-09 19:03:23 +0300

    DatabaseConnection.java

--- JDBC/DatabaseConnection.java
+++ JDBC/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3307/AuthorDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Asli1205?";
+    private static final String USER = "kullanici adi";
+    private static final String PASSWORD = "şifre";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
